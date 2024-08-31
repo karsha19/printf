98cba6707be016b26fce2fa50b41402310048f3e
Karsha <155376011+karsha19@users.noreply.github.com>
2024-08-31 12:35:49 +0530

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /************************* PRINT UNSIGNED NUMBER *************************/
-/**
+/*
  * print_unsigned - Prints an unsigned number
- * @types: List a of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
@@ -36,9 +36,9 @@ int print_unsigned(va_list types, char buffer[],
 }
 
 /************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
-/**
+/*
  * print_octal - Prints an unsigned number in octal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
@@ -78,9 +78,9 @@ int print_octal(va_list types, char buffer[],
 }
 
 /************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
-/**
+/*
  * print_hexadecimal - Prints an unsigned number in hexadecimal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
@@ -96,9 +96,9 @@ int print_hexadecimal(va_list types, char buffer[],
 }
 
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
-/**
+/*
  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
- * @types: Lista of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
@@ -114,9 +114,9 @@ int print_hexa_upper(va_list types, char buffer[],
 }
 
 /************** PRINT HEXX NUM IN LOWER OR UPPER **************/
-/**
+/*
  * print_hexa - Prints a hexadecimal number in lower or upper
- * @types: Lista of arguments
+ * @types: List of arguments
  * @map_to: Array of values to map the number to
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
